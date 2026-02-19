310728fc4c7bc945ebf78fdff2d4c042493e2a5d
Additya Pramanik <addityapramanik@gmail.com>
2026-02-19 19:45:01 +0530

    Basic --- 1

---
+++ JavaScript/app.js
@@ -0,0 +1,52 @@
+console.log("Hello! ");
+alert("Something Bizzare!");
+console.log(2*2);
+console.warn('Butter');
+console.table({Hero:"Me", Zero:"You"});
+// console.clear();
+//log--console.log();
+// (reserved keyword) (variable name) (type of value)
+
+//declared variable
+let name1;
+
+//assign value
+let water= "20Water"
+name1= "Padding";
+console.log(name1);
+console.log(water);
+name1= "MKO";
+console.log(name1);
+const bobo = "k";
+console.log(bobo);
+
+// bobo="l";
+// console.log(bobo);  ---will give error 
+
+// const bon ;  ---will give error
+
+var vf;
+vf= 2;
+console.log(vf);
+vf=3;
+console.log(vf);
+let yf = 4;
+console.log(yf);
+console.log(typeof vf);
+console.log(typeof yf);
+console.log(++yf);
+console.log(yf++);
+console.log(yf);
+
+console.log(yf ** vf); //wiill run perfectly
+console.group(2**5);
+
+
+
+
+
+
+
+
+
+
