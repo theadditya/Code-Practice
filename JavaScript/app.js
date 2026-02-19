3cf87b40800af43dc658b2e13ba5466f3cf3163d
Additya Pramanik <addityapramanik@gmail.com>
2026-02-19 22:37:06 +0530

    Basic ---String

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -72,6 +72,81 @@ console.log(10 !== 10);
 console.log(10 != "10");
 
 
+//Strrings
+let go ="Hello";
+let no ='Hello';
+let fo = `Hello`;
+console.log(go);
+console.log(no);
+console.log(fo);
+let gofono= go + " "+ fo +" "+ no;
+console.log(gofono);
+
+//1. Cconcatenation
+let gofo = go.concat(fo);
+console.log(gofo);//HelloHello
+
+let gonofo= go.concat(fo).concat(no);
+console.log(gonofo);//HelloHelloHello
+
+//2. Append
+go += " World!"
+fo = fo +" World!"
+console.log(go);
+console.log(fo);
+
+//3. Legth 
+console.log(go.length);
+
+//4.Cases
+console.log(go);
+console.log(go.toUpperCase());
+console.log(go.toLowerCase());
+
+//5.Slice
+console.log(go.slice(0,5));
+
+
+//6.Split & Join
+console.log(go.split(""));
+console.log(go.split(" "));
+console.log(go.split(" ").join(""));
+console.log(go.split(" ").join(" "));
+
+//7.Includes
+console.log(go.includes('Z'));
+console.log(go.includes('H'));
+console.log(gofono.trim());
+
+//8.Backticks
+let backt=`hello There !
+its Me Adiitya...
+hoping you are doing great...;)`
+console.log(backt);
+
+let biuy= ` ${go}, ${fo}, Hello There`
+console.log(biuy);
+
+///Tpe Conversion
+let g = "69";
+
+//convert string to umber
+g= parseInt(g);
+console.log(g);
+console.log(typeof g);
+
+ 
+// number to string 
+g=  g.toString();
+console.log(g);
+console.log(typeof g);
+
+
+
+
+
+
+
 
 
 
