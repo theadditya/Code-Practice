bf59df890d1e01abb305f362f78bf380b9d17229
Additya Pramanik <addityapramanik@gmail.com>
2026-02-22 22:50:55 +0530

    Basic ---For-Of-Loop

---
+++ For-Of-Loop/app.js
@@ -0,0 +1,23 @@
+//For.. Of loop 
+//a concise way to lop over elements in literable objects like
+// arrays,strings,maps,sets,and more. It allows you toiterate 
+//directly over the values ofthe elemets, rather than dealing
+//with their indicies or keys, which makes the code more readable
+//ad less error-prone.
+
+//for (variables of literables) {...}
+
+let peoples = ["asii",'ale',"ri","prif"];
+
+for (let people of peoples){
+    console.log(people);
+    console.log(peoples);
+    
+    
+}
+
+const text = "BOJIKIYTF"
+for(let chara of text){
+    console.log(chara);
+    
+}
